11688273a1089f80966aa63c51d931b1cf846928
ecostrong <necoarmstrong@mac.com>
2022-04-14 09:12:36 -0700

    unfinished from yesterday

---
+++ intro-natalia-neco.rb
@@ -0,0 +1,58 @@
+# Challenges
+# Arithmetic Challenges
+# Remember that floats are numbers with a decimal point whereas integers 
+# have no fractional part so in Ruby, 1 and 1.0 are different types of things with slightly different behaviors.
+
+# Add, subtract, multiply and divide some numbers and floats.
+# p 1+1 
+# p 10+12 
+# p 34 - 56
+# p 26 * 4
+# p 15/3
+# p 15 % 3
+# p 6**3
+# p 3.0/2
+# p 0.16 + 0.49
+
+# Find the remainder of dividing two numbers using the modulo operator (%).
+# p 21 % 3
+
+# Divide a number by 0. 
+# p 25/0
+# Divide 0 by 0.
+#p 0/0
+
+# Variables Challenges
+# Set a variable called my_favorite_number equal to your favorite number
+
+my_favorite_number = 22
+
+# Calculate your favorite number divided by 2
+p my_favorite_number / 2
+
+# Set another variable called someones_favorite equal to 13
+
+# someones_favorite = 13 
+
+# Change the value of someones_favorite to 7
+
+# someones_favorite = 7
+
+
+# Subtract my_favorite_number from someones_favorite
+# Change the value of my favorite number to be 26 times its current value
+# Strings Challenges
+# Create a variable and return it in a sentence string interpolation
+# Create a variable that contains a string and test some of the Ruby string methods
+# .upcase
+# .reverse
+# .include?
+# .capitalize
+# Arrays Challenges
+# Create an array that contains the name of at least five TV shows
+# Find the length of the array
+# Return the first item in the array
+# Return the fourth item in the array
+# Permanently reverse the order of the array
+# Create a new empty array for your top favorite TV shows
+# From the full TV show array, add your top two favorite shows to the empty array
