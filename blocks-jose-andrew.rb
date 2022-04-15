bd4d6523ba96096ae9b25035c9cf4d2243f1fc1a
joagui83 <jjaggie83@gmail.com>
2022-04-15 11:40:41 -0700

    committing

--- blocks-jose-andrew.rb
+++ blocks-jose-andrew.rb
@@ -25,3 +25,39 @@
 # end
 
 # p show_evens [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
+
+#Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+
+# numbers = [2,23,45,68,120,300]
+
+# mult_arr = numbers.map do |value|
+#   value * 5
+# end
+# p mult_arr 
+
+#Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+# arr = ['jose', 'andrew']
+# def cap_words cap 
+#     cap.map(&:capitalize)
+# end
+
+# p cap_words arr
+
+# # arr = ['jose', 'andrew']
+# # def cap_words cap 
+# #     cap.map(&:capitalize) do |value|
+# #     value
+# #     end
+
+# # end
+
+# Create a method that takes in an array of numbers and returns the largest number in the array.
+
+# arr = [1,3,4,5,6]
+# def largest_num large
+#     large.max
+
+# end 
+# p largest_num arr
+
+# Create a method that takes in an array of numbers and returns the smallest number in the array.
