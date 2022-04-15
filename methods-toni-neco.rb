418adaff3411d4dc6856fababe75a6267db1a0a1
GeeeShep <gashep@yahoo.com>
2022-04-14 21:24:56 -0700

    initial challenges completed

---
+++ methods-toni-neco.rb
@@ -0,0 +1,50 @@
+# Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+
+
+
+
+
+# def sum_these_numbers(num1, num2)
+#     num1 + num2 
+# end
+# p sum_these_numbers(5,7)
+
+# output---> 12
+
+# --------------------
+# Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+
+
+
+# def is_even = gets.chomp
+# if is_even % 2 == 0
+#   puts "TRUE"
+# else
+#   puts "FALSE"
+# end
+
+# ---------------------------
+# Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+
+# def all_inclusive(num1)
+#     if num1 >= 1 && num1 <= 10
+#         p 'Valid'
+#     else
+#         p 'Invalid'
+#     end
+# end
+# all_inclusive 12
+
+------------------------------------
+
+
+# Create a method that takes in a string and determines if the string is a palindrome.
+
+# def palindrome(madam)
+#     if madam == madam.reverse
+#         p 'true'
+#     else
+#         p 'false'
+#     end
+# end
+
