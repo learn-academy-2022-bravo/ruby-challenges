b741c901ab9a174949d624dc59e1e940d8805dfe
lundrew <andrueee@gmail.com>
2022-04-15 11:03:03 -0700

    Completed Challenges

---
+++ blocks-jose-andrew.rb
@@ -0,0 +1,27 @@
+# # Write a loop that prints the numbers 1 to 20.
+
+# num = 0
+# while num < 20
+#     p num += 1
+# end
+
+# Write a loop that prints the numbers 1 to 20 using a different approach than previously used.
+
+# nums_arr = []
+# num = 1
+# while num < 21
+#   nums_arr << num 
+#   num += 1
+# end
+
+# p nums_arr
+
+# Write a loop that prints only even numbers from 20 to 0.
+
+# def show_evens nums
+#   nums.select do |value|
+#     value.even?
+#   end
+# end
+
+# p show_evens [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
