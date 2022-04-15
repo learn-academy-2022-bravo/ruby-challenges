75a042f9ffd0ed119b2e6d2b7ec20bb5fa493740
ljkotalik <leannak14@gmail.com>
2022-04-15 11:34:14 -0700

    initial commit

---
+++ blocks-neco-lj.rb
@@ -0,0 +1,84 @@
+# Write a loop that prints the numbers 1 to 20.
+# num = 1 
+# while num <=20
+#     p num
+#     num += 1
+# end
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+
+
+
+# Write a loop that prints the numbers 1 to 20 using a different approach than previously used.
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# nums.each do |value| value
+#     p value
+# end
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+
+
+
+# Write a loop that prints only even numbers from 20 to 0.
+num = 20 
+while num >=0
+    if num % 2 == 0
+    p num
+    num -= 1
+    end
+end
+
+
+# Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+# Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+# Create a method that takes in an array of numbers and returns the largest number in the array.
+# Create a method that takes in an array of numbers and returns the smallest number in the array.
+# Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+# Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
+# Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. With the same method, given the letter 'm' should return ['Mango'].
+# FIZZBUZZ
+# Write a method that prints the numbers from 1 to 100. For multiples of three print Fizz instead of the number, for multiples of five print Buzz instead of the number, for numbers that are multiples of both three and five print FizzBuzz, for all other numbers print the number.
+
+
+# num_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+# def is_even array
+#     array.select { |value| value.even? }
+# end
+# p is_even num_array
+# [2, 4, 6, 8, 10, 12, 14, 16, 18]
